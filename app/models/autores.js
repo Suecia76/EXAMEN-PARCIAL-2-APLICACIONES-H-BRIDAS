a73b0aca9067c4bdd3d995763844387e75a1cfdb
Suecia76 <carrascosanti76@gmail.com>
2024-11-27 15:40:15 -0300

    Eliminando carpeta app del repositorio

--- app/models/autores.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const AutoresSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  edad: { type: String, required: true },
-  libros: [{ type: String, required: true }],
-});
-
-export default mongoose.model("autor", AutoresSchema);
